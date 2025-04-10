5458466124d1a81419ac95337d66468952531d8e
MayB-04 <maybadenhorst@gmail.com>
2025-04-10 20:18:54 +0200

    navigation added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import { getCurrentUser, googlePopupAuth, googleSignout } from "./auth/auth";
+import {useRouter} from "next/navigation"
 
 export default function Home() {
+  const router = useRouter();
+
   async function signinClick() {
     const accessToken = await googlePopupAuth();
 
@@ -11,6 +14,7 @@ export default function Home() {
     const user = getCurrentUser();
 
     console.log(user);
+    router.push("/freelancer"); //will have to make dynamic to go to seperate pages
   }
 
   function signoutClick() {
@@ -22,12 +26,14 @@ export default function Home() {
 
     console.log("User", user);
   }
+  
 
   return (
-    <main>
-      <button onClick={signinClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Sign in with google</button>
-      <button onClick={signoutClick} className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Sign in with google</button>
-      <button onClick={currentUserClick} className="bg-orange-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Sign in with google</button>
-    </main>
+    <body>
+      <h1 className = "heading">TaskNet</h1>
+        <button onClick={signinClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Sign in with google</button>
+        <button onClick={signoutClick} className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Sign out</button>
+        <button onClick={currentUserClick} className="bg-orange-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Sign in with google</button>
+     </body>
   );
 }
