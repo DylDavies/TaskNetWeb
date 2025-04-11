805dc97df8097c304db98b4292739bd1f2933ee3
MayB-04 <maybadenhorst@gmail.com>
2025-04-11 17:31:22 +0200

    updating client UI

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,20 +1,44 @@
 import Link from "next/link";
+import Header from "../components/Header/header";
+import WelcomeCard from "../components/WelcomeCard/WelcomeCard";
+import "../components/WelcomeCard/WelcomeCard.css";
+import SideBar from "../components/sidebar/SideBar";
+import "../components/sidebar/sidebar.css";
+
+const links = [{ name: "Client", href:"/client" }, { name: "Logout",href:"/" },{name: "Home",href:"/"}, {name: "Client",href:"/client"}, {name: "Admin",href:"/admin"}];
+
 
 export default function Page(){
     return(
         <>
-            <h1>This is the admin page</h1>
-            <nav>
-                <Link href="/freelancer">link to freelancer</Link> {" "}
-                <br></br>
-                <Link href="/">link to Home</Link> {" "}
-                <br></br>
-                <Link href="/client">link to client</Link> {" "}
-                <br></br>
-                <Link href="/admin">link to admin</Link> {" "}
-            </nav>
+        <body className="min-h-screen flex flex-col dark:bg-[#27274b] text-white font-sans">
+            
+            <header className="w-full bg-orange-500 ">
+                <Header usertype={"client"} name={"Alex"} />
+            </header>
+
+            <main className="flex-1 flex dark:bg-[#cdd5f6]">
+                <section className="w-64">
+                    <SideBar items={links}/>
+
+                </section>
+                <section className="flex-1 p-4 flex items-start justify-center">
+                    <WelcomeCard username="Alex" type="client" />
+                </section>
+                
+
+            </main>
+
+            
+            <footer className="bg-[#f75509] py-4 flex justify-center">
+                <nav className="space-x-8 text-center">
+                    <Link href="/freelancer" className="hover:text-[#1dbf73]">Freelancer</Link>
+                    <Link href="/" className="hover:text-[#1dbf73]">Home</Link>
+                    <Link href="/client" className="hover:text-[#1dbf73]">Client</Link>
+                    <Link href="/admin" className="hover:text-[#1dbf73]">Admin</Link>
+                </nav>
+            </footer>
+        </body>
         </>
-        
     );
-    
 }
--- src/app/components/PendingCard/PendingCard.tsx
+++ src/app/components/PendingCard/PendingCard.tsx
@@ -14,7 +14,7 @@ const PendingCard = () => {
                   <svg className="w-6 h-6 mr-1" fill="currentColor" viewBox="0 0 20 20">
                     <path
                       d="M6 2a1 1 0 000 2h1v1.586A2 2 0 007.586 7L10 9.414V10.5a1 1 0 01-.293.707L7.586 13a2 2 0 00-.586 1.414V16H6a1 1 0 100 2h12a1 1 0 100-2h-1v-1.586a2 2 0 00-.586-1.414l-2.121-2.121A1 1 0 0114 10.5v-.586L16.414 7A2 2 0 0017 5.586V4h1a1 1 0 100-2H6z"
-                    ></path> 
+                    ></path>
                   </svg>
                 </div>
                 <div>
