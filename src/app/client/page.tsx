46d351754d5f356412d712bdf988bd7a6fd961f1
MayB-04 <maybadenhorst@gmail.com>
2025-04-11 10:48:59 +0200

    client page has a bit of a UI now

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -1,29 +1,44 @@
 import Link from "next/link";
-import "./client.css";
-import "../globals.css";
+import Heading from "../components/Heading/heading";
+import WelcomeCard from "../components/WelcomeCard/WelcomeCard";
+import "../components/WelcomeCard/WelcomeCard.css";
+import SideBar from "../components/sidebar/SideBar";
+import "../components/sidebar/sidebar.css";
+
+const links = [{ name: "Client", href:"/client" }, { name: "Logout",href:"/" },{name: "Home",href:"/"}, {name: "Client",href:"/client"}, {name: "Admin",href:"/admin"}];
 
 
 export default function Page(){
     return(
         <>
-            <body className = "client-body">
+        <body className="min-h-screen flex flex-col dark:bg-[#27274b] text-white font-sans">
+            
+            <header className="w-full bg-orange-500 ">
+                <Heading />
+            </header>
 
-                <img src="./client/Logo.png" alt="Logo" className="logo"></img>
-                <h1>Hello Client</h1>
+            <main className="flex-1 flex dark:bg-[#cdd5f6]">
+                <section className="w-64">
+                    <SideBar items={links}/>
 
-            <footer className = "client-footer">
-            <nav>
-                <Link href="/freelancer">link to freelancer</Link> {" "}
-                <br></br>
-                <Link href="/">link to Home</Link> {" "}
-                <br></br>
-                <Link href="/client">link to client</Link> {" "}
-                <br></br>
-                <Link href="/admin">link to admin</Link> {" "}
-            </nav>
+                </section>
+                <section className="flex-1 p-4 flex items-start justify-center">
+                    <WelcomeCard username="May" type="client" />
+                </section>
+                
+
+            </main>
+
+            
+            <footer className="bg-[#f75509] py-4 flex justify-center">
+                <nav className="space-x-8 text-center">
+                    <Link href="/freelancer" className="hover:text-[#1dbf73]">Freelancer</Link>
+                    <Link href="/" className="hover:text-[#1dbf73]">Home</Link>
+                    <Link href="/client" className="hover:text-[#1dbf73]">Client</Link>
+                    <Link href="/admin" className="hover:text-[#1dbf73]">Admin</Link>
+                </nav>
             </footer>
-
-            </body>
+        </body>
         </>
-    )
+    );
 }
