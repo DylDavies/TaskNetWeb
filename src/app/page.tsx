19fb1d294c958449330a42ab9761b82b77dacab2
Sudhir Ajodhya <62798738+SudhirAjodhya@users.noreply.github.com>
2025-04-12 00:10:53 +0200

    removed merge comments on page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,28 @@
 "use client";
 
 import AuthService from "./services/AuthService";
-//<<<<<<< feature/admin
-//import Button from "./components/button/Button";
-//>>>>>>> dev
 import React from "react";
-//=======
-//import Button from "./components/button/Button";
-//import React, { useState } from "react";
-/*import SideBar from "./components/sidebar/SideBar";*/
-//>>>>>>> dev
 import "./components/sidebar/sidebar.css";
 import "./components/button/Button.css";
-//import InputBar from "./components/inputbar/InputBar";
-//import "./components/inputbar/inputBar.css";
-//import SearchBar from "./components/searchbar/SearchBar";
-//import "./components/searchbar/SearchBar.css";
-//import SideBar from "./components/sidebar/SideBar";
 import PendingCard from "./components/PendingCard/PendingCard";
 import Header from "./components/Header/header";
 
 export default function Home() {
-//<<<<<<< feature/admin
- //const [inputValue, setInputValue] = useState("");
-
-  //async function signinClick() {
-    //const accessToken = await googlePopupAuth();
-    //console.log(accessToken);
-    //const user = getCurrentUser();
-    //console.log(user);
-//=======
   AuthService.autoSignIn();
 
   function signinClick() {
     AuthService.signin();
-//>>>>>>> dev
   }
 
   function signoutClick() {
     AuthService.googleSignout();
   }
 
-/*<<<<<<< feature/admin
-  function currentUserClick() {
-    const user = getCurrentUser();
-=======*/
   async function currentUserClick() {
     const user = await AuthService.getCurrentUser();
 
-//>>>>>>> dev
     console.log("User", user);
   }
-
-  //function Clickable() {
-    //console.log("Button clicked!");
-  //}
-
-  //const items = [{ name: "Client" }, { name: "Logout" }];
-
- // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-   // setInputValue(e.target.value);
- // };
 
   return (
     <main>
