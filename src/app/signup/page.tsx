5baef2444021bb0ea39356069c7f815932889eb3
kaylajulie <kaylajschwarz@gmail.com>
2025-04-13 17:04:48 +0200

    ui signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,19 +1,26 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import InputBar from "../components/inputbar/InputBar";
 import "../components/inputbar/inputBar.css";
 import Button from "../components/button/Button";
 import "../components/button/Button.css";
 import "./global.css";
 
+
+
 export default function Page() {
+    const [inputText, setInputText] = useState("");
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+    };
     return(
         <main className="flex items-center justify-center h-screen bg-neutral-900">
             <section className="bg-white rounded-2xl shadow-2xl w-full max-w-4xl flex overflow-hidden">
                 {/* Left side (Image section) */}
                 <section className="w-1/2 bg-neutral-900 text-white flex justify-center items-center">
                     <img
-                        src="/images/signup.jpg"
+                        src="/images/signup-freelance.jpg"
                         className="w-full h-full object-cover"
                         alt="Sign Up"
                 />
@@ -31,11 +38,12 @@ export default function Page() {
                         <section className="flex items-center gap-4">
                             <section className="w-1/3 text-right text-lg opacity-80">Username</section>
                             <section className="w-2/3">
-                                <input
-                                    type="text"
-                                    placeholder="Enter username"
-                                    className="w-full p-2 rounded text-black"
-                                />
+                            <InputBar
+                                value= "Enter your username"
+                                onChange={handleInputChange}
+                                placeholder="Enter your username"
+                                type="username"
+/>
                             </section>
                         </section>
 
