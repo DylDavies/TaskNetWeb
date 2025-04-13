09840b45f3f320c8006f7f78d47474757161cdf0
Dylan Davies <34412943+DylDavies@users.noreply.github.com>
2025-04-13 12:26:18 +0200

    fix: environment issues

--- src/app/services/ApiService.ts
+++ src/app/services/ApiService.ts
@@ -1,5 +1,5 @@
 export default class ApiService {
-    static BASE_URL = process.env.API_URL;
+    static BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
     static async sessionExists(): Promise<{presence: boolean, customToken?: string}> {
         try {
