8680fe0d914e826dcdd183cce623591d691f2377
Dylan Davies <34412943+DylDavies@users.noreply.github.com>
2025-04-13 21:00:53 +0200

    fix: don't always redirect to admin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   AuthService.autoSignIn();
 
   async function signinClick() {
-    AuthService.signin("/admin");
+    AuthService.signin();
     const activeUser = await AuthService.getCurrentUser();
     
     //if there is a user, will update the username
