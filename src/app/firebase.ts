2559b587b7ae3fe018f0f0fe5a92f05e2ab93496
MichaelHB04 <michael.h.badenhorst@gmail.com>
2025-04-17 15:05:50 +0200

    adding a function that uploads a file to the database as well as the application interface

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage, ref } from 'firebase/storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDYgCHlflSVMDNxn3WJzoWCc1tOLBZUULo",
@@ -13,5 +14,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const storage = getStorage(app);
+const sotrageRef = ref(storage);
 
-export { app, db };
+export { app, db, storage };
