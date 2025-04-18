0688b48e5194de60c27223b8b4b88986b6d9dc53
SudhirAjodhya <sudhir.ajodhya@gmail.com>
2025-04-18 17:11:56 +0200

    Removed Tests from freelancer & added link to job Search Page

--- src/app/freelancer/page.tsx
+++ src/app/freelancer/page.tsx
@@ -7,21 +7,20 @@ import "../components/sidebar/sidebar.css";
 import Button from "../components/button/Button";
 import "../components/button/Button.css";
 import "./global.css";
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext } from "react";
 import AuthService from "../services/AuthService";
 import { useRouter } from "next/navigation";
 import { AuthContext, AuthContextType } from "../AuthContext";
 import JobData from "../interfaces/JobData.interface";
-import MultiSelect from "../components/MultiSelectBar/MultiSelectBar";
-import { getAllSkills } from "../server/services/SkillsService";
 
 //constant for links to other pages
-const links = [{ name: "Home", href: "/" }];
+const links = [
+  { name: "Home", href: "/" },
+  { name: "Find Jobs", href: "/jobSearch" },
+];
 
 export default function Page() {
   const [jobData] = useState<JobData | null>(null);
-  const [skills, setSkills] = useState<string[]>([]);
-  const [selectedSkills, setSelectedSkills] = useState<string[]>([]); // Track selected skills
   const { user } = useContext(AuthContext) as AuthContextType;
   const router = useRouter();
 
@@ -30,24 +29,6 @@ export default function Page() {
     AuthService.googleSignout();
     router.push("/");
   }
-
-  useEffect(() => {
-    async function fetchSkills() {
-      try {
-        const skillData = await getAllSkills(); // this returns all skills as one flat array
-        setSkills(skillData);
-      } catch (err) {
-        console.error("could not fetch skillData: ", err);
-      }
-    }
-
-    fetchSkills();
-  }, []);
-
-  useEffect(() => {
-    // Sanity check
-    //console.log("Selected Skills:", selectedSkills);
-  }, [selectedSkills]);
 
   return (
     <section className="min-h-screen flex flex-col dark:bg-[#27274b] text-white font-sans">
@@ -68,12 +49,6 @@ export default function Page() {
         <section>
           <p>{jobData?.title}</p>
         </section>
-
-        {/* MultiSelect for selecting skills */}
-        <MultiSelect
-          skills={skills}
-          onSelect={setSelectedSkills} // Pass the function to update selected skills
-        />
 
         {/* Welcome card section */}
         <section className="flex-1 p-4 flex items-start justify-center">
