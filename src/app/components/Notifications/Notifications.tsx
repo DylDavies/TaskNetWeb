2b5d5ae71f91e98a7648cb94abde43c415ddf380
Dylan Davies <dyldavies11@gmail.com>
2025-04-18 11:03:26 +0200

    fix: non semantic element

--- src/app/components/Notifications/Notifications.tsx
+++ src/app/components/Notifications/Notifications.tsx
@@ -40,11 +40,11 @@ const Notifications = () => {
           />
         </svg>
         { displayBadge ?
-        <span
+        <strong
           aria-label="Notification Badge"
           role="status"
           className="absolute top-0 right-0 inline-block w-3 h-3 transform translate-x-1 -translate-y-1 bg-red-600 border-2 border-white rounded-full dark:border-gray-800"
-        ></span>
+        ></strong>
         :
         <></> }
       </button>
