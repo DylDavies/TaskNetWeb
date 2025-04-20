7732ccd06454de063b6274b04ecb9a77668a10f6
SudhirAjodhya <sudhir.ajodhya@gmail.com>
2025-04-20 21:53:13 +0200

    added and cleaned up formatters

--- src/app/server/formatters/FormatDates.ts
+++ src/app/server/formatters/FormatDates.ts
@@ -33,4 +33,15 @@ const formatDateAsDate = (dateNum?: number): string => {
     });
 };
 
-export {formatDateAsDate, formatDateAsString}
+function formatDateAsNumber(date: Date): number {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Months are 0-indexed
+  const day = date.getDate().toString().padStart(2, "0");
+
+  return parseInt(`${year}${month}${day}`, 10);
+}
+
+export default formatDateAsNumber;
+
+
+export {formatDateAsDate, formatDateAsString, formatDateAsNumber}
