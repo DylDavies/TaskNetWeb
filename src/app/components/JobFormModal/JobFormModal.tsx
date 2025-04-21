a0065f3083a3d6d6144fd8270e58309c95a505e1
Swaitj <jadeswait123@gmail.com>
2025-04-21 11:59:30 +0200

    added functionality to the JobFormModal

--- src/app/components/JobFormModal/JobFormModal.tsx
+++ src/app/components/JobFormModal/JobFormModal.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, {useContext} from "react";
 import Modal from "react-modal";
 import './JobApplicationForm.css';
 import Button from "../button/Button";
+import { AuthContextType, AuthContext } from "../../AuthContext";
+import { AddApplication, uploadCV }from "@/app/server/services/ApplicationService";
 
 type JobData = {
   company: string;
   jobTitle: string;
-  budget: string;
-  deadline: string;
-  skills: string[];
+  jobId: string;
 };
 
 type Props = {
@@ -16,7 +16,29 @@ type Props = {
 }
 
 const JobForm: React.FC<Props> = ({data}) => {
+    const { user } = useContext(AuthContext) as AuthContextType;
+    const [applicantID, setApplicantID] = React.useState("");
+    const [bidAmount, setBidAmount] = React.useState(0);
+    const [CV, setCV] = React.useState<File | null>(null);
+    const [estismatedTimeline, setEstismatedTimeline] = React.useState(0);
+    const [jobID, setJobID] = React.useState("");
+    const [CVURL, setCVURL] = React.useState("");
     const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    const handleUpload = async () => {
+      const url = await uploadCV(CV!, applicantID);
+      setCVURL(url);
+    }
+
+    const handleApplicationSubmit = (e: React.FormEvent) => 
+    {
+      e.preventDefault();
+      setApplicantID(user?.authUser.uid!);
+      handleUpload();
+      setJobID(data.jobId);
+      AddApplication(applicantID, bidAmount, CVURL, estismatedTimeline, jobID);
+      closeModal();
+    }
     
     /*function openModal(){
         setIsOpen(true);
@@ -25,11 +47,6 @@ const JobForm: React.FC<Props> = ({data}) => {
     function closeModal(){
         setIsOpen(false);
     }
-
-    /*function submitForm(jobData: jobData){
-      createJob(jobData);
-      setIsOpen(false);
-    }*/
 
   return (
     <section>
@@ -40,14 +57,14 @@ const JobForm: React.FC<Props> = ({data}) => {
         ariaHideApp={false}>
             <form>
             <button type="button" onClick={closeModal} className="Close">X</button> <br></br>
-            <header>Job Application Form for {data.jobTitle}</header> <br></br>
+            <header>Job Application Form for {data.jobTitle} by {data.company}</header> <br></br>
             <label htmlFor="pdf">Please attach your cover letter</label><br></br>
-                <input type="file" accept=".pdf" id ="pdf" name="pdf"></input><br></br>
+                <input type="file" accept=".pdf" id ="pdf" name="pdf" onChange={(e) => {const file = e.target.files?.[0]; if(file){setCV(file);}}}></input><br></br>
                 <label htmlFor="timeLine">Please write down your estimated time line</label><br></br>
-                <input type="date" id="timeLine" name="timeLine"></input><br></br>
+                <input type="date" id="timeLine" name="timeLine" value ={estismatedTimeline} onChange={(e) => setEstismatedTimeline(Number(e.target.value))}></input><br></br>
                 <label htmlFor="bid">Please write down your bid</label><br></br>
-                <input type="text" id="bid" name ="bid"></input><br></br>
-                <Button caption = {"Submit"} onClick={closeModal}></Button>
+                <input type="text" id="bid" name ="bid" value={bidAmount} onChange={(e) => setBidAmount(Number(e.target.value))}></input><br></br>
+                <Button caption = {"Submit"} onClick={() => handleApplicationSubmit}></Button>
             </form>
     </Modal>
     </section>
