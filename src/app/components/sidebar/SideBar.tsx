d7445094516699d15f3bfd3900b41f201fdabcd8
MayB-04 <maybadenhorst@gmail.com>
2025-05-04 21:04:08 +0200

    Making the search bar look better on Admin page and routing fixing

--- src/app/components/sidebar/SideBar.tsx
+++ src/app/components/sidebar/SideBar.tsx
@@ -1,48 +1,16 @@
-import React, { JSX } from "react";
+import React from "react";
 import Link from "next/link";
 import "./sidebar.css";
 import Button from "../button/Button";
 import AuthService from "@/app/services/AuthService";
 import { useRouter } from "next/navigation";
 
-/*
-------NOTE TO USER OF THIS COMPONENT-----
-  This component dynamically renders items onto the sidebar 
-  using list components, to do so:
-
-  Create a list of item name objects for example:
-
-  const items = [{ name: "Client" }, { name: "Logout" }];
-
-  and create the component:
-
-  <SideBar items={items} />
-
-  passing in the props
-
-*/
-
 type SideBarProps = {
-  items: { name: string; href: string, selected: boolean }[];
-  myfunction?: JSX.Element ;
-}
+  items?: { name: string; href: string; selected: boolean }[];
+  myfunction?: React.ReactNode;
+};
 
-/*
-
-  Sidebar is a react function component that expects props as its input, from those props were extracting items
-  React.FS<Props> : functional component that takes in props of type 'Props'
-  = ({ items }) => {...} is destructuring
-  
-  its equivalent to:
-  const SideBar = (prop:Props) => {
-    const { items } = props;
-  }
-
-
-  "Create a functional component named SideBar that receives props shaped like Props, and we’re pulling items straight out from the props."
-
-*/
-const SideBar =  ({ items ,myfunction}: SideBarProps ) => {
+const SideBar = ({ items, myfunction }: SideBarProps) => {
   const router = useRouter();
 
   function signoutClick() {
@@ -56,29 +24,37 @@ const SideBar =  ({ items ,myfunction}: SideBarProps ) => {
         <a className="ml-6 text-lg font-bold text-gray-200" href="#">
           {/* Optional: Sidebar Title */}
         </a>
+      
         <ul className="mt-6 list">
-          {items.map((item, index) => (
+          {items?.map((item, index) => (
             <li key={index}>
-              {item.selected ?
-              (<Link href={item.href} className="block px-4 py-2 text-white bg-gray-700 rounded pointer-events-none" 
-                aria-disabled={true}
-                tabIndex={-1}
-              >
-                {item.name}
-              </Link>) : (
-              <Link href={item.href} className="block px-4 py-2 text-gray-300 hover:text-white hover:bg-gray-700 rounded">
-                {item.name}
-              </Link>
+              {item.selected ? (
+                <Link
+                  href={item.href}
+                  className="block px-4 py-2 text-white bg-gray-700 rounded pointer-events-none"
+                  aria-disabled={true}
+                  tabIndex={-1}
+                >
+                  {item.name}
+                </Link>
+              ) : (
+                <Link
+                  href={item.href}
+                  className="block px-4 py-2 text-gray-300 hover:text-white hover:bg-gray-700 rounded"
+                >
+                  {item.name}
+                </Link>
               )}
             </li>
           ))}
               
-        {myfunction &&(
-          <li  className="block px-4 py-2 text-gray-300 hover:bg-gray-700 rounded hover-effect">
-            {myfunction}
-        </li>
-        )}
+          {myfunction && (
+            <li className="block px-4 py-2 text-gray-300 hover:bg-gray-700 rounded hover-effect">
+              {myfunction}
+            </li>
+          )}
         </ul>
+        
         <section className="absolute bottom-0 mb-4">
           <Button caption={"Log out"} onClick={() => signoutClick()} />
         </section>
